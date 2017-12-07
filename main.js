76612ac3e6085430ead0492b4a0987e154796bb3
Peter <petecruckshank@gmail.com>
2017-12-06 21:12:56 -0500

    Created new file main.js and removed my script from index.html

---
+++ main.js
@@ -0,0 +1,15 @@
+var FCC_Stream, Noobs2_Stream;
+
+$(document).ready(function(){
+
+ $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
+FCC_Stream = data;
+var logo = data.logo;
+
+$("#FCClink").attr("href" , data.url);
+$("#FCClogo").attr("src", logo);
+console.log(logo);
+console.dir(FCC_Stream);
+}); // end of .getJSON() function
+
+}); // end of .ready() function
