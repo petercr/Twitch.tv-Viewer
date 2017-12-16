991f18663446d6adcabea4c55c41ec109a221181
Peter Cruckshank <petecruckshank@gmail.com>
2017-12-15 20:28:49 -0500

    tried to stylize main.js with beautify.js

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 var FCC_Stream, Noobs2_Stream;
 
-function ready(fn) {
-	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-		fn();
-	} else {
-		document.addEventListener('DOMContentLoaded', fn);
-	}
+(function() {
 
 
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
-		FCC_Stream = data;
-		var logo = data.logo;
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?", function(data) {
+        FCC_Stream = data;
+        var logo = data.logo;
 
-		$("#FCClink").attr("href" , data.url);
-		$("#FCClogo").attr("src", logo);
-		console.log(logo);
-		console.dir(FCC_Stream);
-	}); // end of .getJSON() function
+        $("#FCClink").attr("href", data.url);
+        $("#FCClogo").attr("src", logo);
+        console.log(logo);
+        console.dir(FCC_Stream);
+    }); // end of .getJSON() function
 
-}
+});
