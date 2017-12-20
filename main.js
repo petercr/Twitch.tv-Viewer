d4404cfff04f376f890a36d5cb93ee1f860a7b72
Peter Cruckshank <petecruckshank@gmail.com>
2017-12-19 20:47:42 -0500

    tried to get fetch() to work. Added in headers from errors

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ var FCC_Stream, Noobs2_Stream;
 ( function() {
 	let mainURL = "https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?";
 
-	$.getJSON( mainURL, function( data ) {
-		FCC_Stream = data;
-		var logo = data.profile_banner;
+	// $.getJSON( mainURL, function( data ) {
+	// 	FCC_Stream = data;
+	// 	var logo = data.profile_banner;
+  //
+	// 	$( "#FCClink" ).attr( "href", data.url );
+	// 	$( "#FCClogo" ).attr( "src", logo );
+	// 	console.log( logo );
+	// 	console.dir( FCC_Stream );
+	// } ); // end of .getJSON() function
 
-		$( "#FCClink" ).attr( "href", data.url );
-		$( "#FCClogo" ).attr( "src", logo );
-		console.log( logo );
-		console.dir( FCC_Stream );
-	} ); // end of .getJSON() function
-
-	fetch( mainURL ).then( function( response ) {
-		return response.json;
+	fetch( mainURL, { method: "GET", mode: "no-cors"} ).then( function( response ) {
+		return response;
 	} )
-		.then( function( json ) {
-			console.dir( json );
+		.then( function( response ) {
+			console.dir( response );
 		} );
 
 } )( );
