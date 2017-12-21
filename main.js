292ed7feca5cad1d3a0141fca6b2f132358aca9a
Peter Cruckshank <petecruckshank@gmail.com>
2017-12-20 20:50:34 -0500

    decided to go back and just use the $.getJSON call for the projet. It's working again

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 var FCC_Stream, Noobs2_Stream;
 
-( function() {
-	let mainURL = "https://wind-bow.gomix.me/twitch-api/channels/freecodecamp";
+(function() {
+    let mainURL = "https://wind-bow.glitch.me/twitch-api/channels/freecodecamp"
 
-	// $.getJSON( mainURL, function( data ) {
-	// 	FCC_Stream = data;
-	// 	var logo = data.profile_banner;
-  //
-	// 	$( "#FCClink" ).attr( "href", data.url );
-	// 	$( "#FCClogo" ).attr( "src", logo );
-	// 	console.log( logo );
-	// 	console.dir( FCC_Stream );
-	// } ); // end of .getJSON() function
+    $.getJSON(mainURL, function(data) {
+        FCC_Stream = data;
+        var logo = data.profile_banner;
 
-	fetch( mainURL, { method: "GET", mode: "no-cors"} ).then( function( response ) {
-		return response.json;
-	} )
-		.then( function( data ) {
-			console.dir( data );
-		} );
+        $("#FCClink").attr("href", data.url);
+        $("#FCClogo").attr("src", logo);
+        console.log(logo);
+        console.dir(FCC_Stream);
+    }); // end of .getJSON() function
 
-} )( );
+    // fetch(mainURL, { method: "GET", mode: "no-cors" }).then(function(response) {
+    //         return response.json;
+    //     })
+    //     .then(function(data) {
+    //         console.dir(data);
+    //     });
+
+})();
