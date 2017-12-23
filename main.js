f71728b10beef0320c6af244141e06212acb10f4
Peter <petecruckshank@gmail.com>
2017-12-23 03:15:07 -0500

    added in a for loop to cycle through CHANNELS and make API calls

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const CHANNELS = ["freecodecamp", "aws", "kyleshevlin", "barbarousking"];
 (function() {
     let mainURL = "https://wind-bow.glitch.me/twitch-api/channels/";
 
+    for (var i in CHANNELS) { // cycle through the CHANNELS and make an API call for all of them
+        $.getJSON(mainURL + CHANNELS[i], function(data) {
+            console.dir(data);
+        }); // end of .getJSON() function
+    }
+
+
     $.getJSON(mainURL + CHANNELS[0], function(data) {
         FCC_Stream = data;
         let logo = data.profile_banner;
