9ada4bb8b4edc6f0b21d3496339684bd859c8d43
Peter Cruckshank <petecruckshank@gmail.com>
2017-12-24 22:05:11 -0500

    applied ESLint spacing instead of Beautify.js extention [VS Code]

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 let FCC_Stream, Noobs2_Stream;
 
-const CHANNELS = ["freecodecamp", "aws", "kyleshevlin", "barbarousking"];
+const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 
-(function() {
-    let mainURL = "https://wind-bow.glitch.me/twitch-api/channels/";
+( function() {
+	let mainURL = "https://wind-bow.glitch.me/twitch-api/channels/";
 
-    for (var i = 0; i < CHANNELS.length; i++ ) { // cycle through the CHANNELS and make an API call for all of them
-        $.getJSON(mainURL + CHANNELS[i], function(data) {
-            console.dir(data);
-            console.log(i);
-            let logo = data.profile_banner;
-            // document.getElementById("link" + i).setAttribute("href", data.url);
-            // document.getElementById(`card${i}`).style.backgroundImage = `url( ${logo})`;
-            
-        }); // end of .getJSON() function
-    }
+	for ( var i = 0; i < CHANNELS.length; i++ ) { // cycle through the CHANNELS and make an API call for all of them
+		$.getJSON( mainURL + CHANNELS[ i ], function( data ) {
+			console.dir( data );
+			console.log( i );
+			let logo = data.profile_banner;
+
+			// document.getElementById("link" + i).setAttribute("href", data.url);
+			// document.getElementById(`card${i}`).style.backgroundImage = `url( ${logo})`;
+
+		} ); // end of .getJSON() function
+	}
 
 
-    $.getJSON(mainURL + CHANNELS[0], function(data) {
-        FCC_Stream = data;
-        let logo = data.profile_banner;
+	$.getJSON( mainURL + CHANNELS[ 0 ], function( data ) {
+		FCC_Stream = data;
+		let logo = data.profile_banner;
 
-        
-        document.getElementById("FCClink").setAttribute("href", data.url);
 
-        document.getElementById("card1").style.backgroundImage = `url( ${logo})`;
-        console.log(logo);
-        console.dir(FCC_Stream);
-    }); // end of .getJSON() function
+		document.getElementById( "FCClink" ).setAttribute( "href", data.url );
 
-    // fetch(mainURL, { method: "GET", mode: "no-cors" }).then(function(response) {
-    //         return response.json;
-    //     })
-    //     .then(function(data) {
-    //         console.dir(data);
-    //     });
+		document.getElementById( "card1" ).style.backgroundImage = `url( ${logo})`;
+		console.log( logo );
+		console.dir( FCC_Stream );
+	} ); // end of .getJSON() function
 
-})();
+	// fetch(mainURL, { method: "GET", mode: "no-cors" }).then(function(response) {
+	//         return response.json;
+	//     })
+	//     .then(function(data) {
+	//         console.dir(data);
+	//     });
+
+} )();
