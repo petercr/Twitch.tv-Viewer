460bf140130735a548bc1ab788899758d9d711b0
Peter Cruckshank <petecruckshank@gmail.com>
2017-12-27 15:17:58 -0500

    played around with getting the data.obj back from API and pasing value

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 
 ( function() {
 	let mainURL = "https://wind-bow.glitch.me/twitch-api/channels/";
-
+	var channelData;
 	for ( var i = 0; i < CHANNELS.length; i++ ) { // cycle through the CHANNELS and make an API call for all of them
-		let channelData;
+
 		console.log( i );
 		$.getJSON( mainURL + CHANNELS[ i ], function( data ) {
 			console.dir( data );
 			channelData = data;
-			
+
 			let logo = data.profile_banner;
 
 			// render new box elements for each channel
@@ -19,23 +19,23 @@ const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 		} ); // end of .getJSON() function
         console.log(channelData);
         let cards = `<div class="cards">
-        <p>${channelData.display_name}</p>
+        <p>${channelData.name}</p>
         <p>stuff</p>`;
-        document.querySelector("#showcase").append(cards);
-	} // end for...loop 
+        document.getElementById("showcase").innerHTML = cards;
+	} // end for...loop
 
 
-	$.getJSON( mainURL + CHANNELS[ 0 ], function( data ) {
-		FCC_Stream = data;
-		let logo = data.profile_banner;
-
-
-		document.getElementById( "FCClink" ).setAttribute( "href", data.url );
-
-		document.getElementById( "card1" ).style.backgroundImage = `url( ${logo})`;
-		console.log( logo );
-		console.dir( FCC_Stream );
-	} ); // end of .getJSON() function
+	// $.getJSON( mainURL + CHANNELS[ 0 ], function( data ) {
+	// 	FCC_Stream = data;
+	// 	let logo = data.profile_banner;
+	//
+	//
+	// 	document.getElementById( "FCClink" ).setAttribute( "href", data.url );
+	//
+	// 	document.getElementById( "card1" ).style.backgroundImage = `url( ${logo})`;
+	// 	console.log( logo );
+	// 	console.dir( FCC_Stream );
+	// } ); // end of .getJSON() function
 
 	// fetch(mainURL, { method: "GET", mode: "no-cors" }).then(function(response) {
 	//         return response.json;
