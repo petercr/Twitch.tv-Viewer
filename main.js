800b920de9b800a32911251f6e9e94b27089edc4
Peter <petecruckshank@gmail.com>
2018-01-02 21:33:20 -0500

    Added the user story requirements as comments in the js file

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// User Story: I can see whether Free Code Camp is currently streaming on Twitch.tv.
+//
+// User Story: I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
+//
+// User Story: if a Twitch user is currently streaming, I can see additional details about what they are streaming.
+
+
 let FCC_Stream, Noobs2_Stream;
 
 const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
