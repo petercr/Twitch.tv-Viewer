7e72a8af3dc86601723f64c6c0820924da72e128
Peter <petecruckshank@gmail.com>
2018-01-18 20:42:31 -0500

    Commented out 1st AJAX call. Use second call to check stream status Also added an eventListener() that opens a window just based on the basic url with the channel added at the end

--- main.js
+++ main.js
@@ -12,58 +12,59 @@ const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 ( function() {
 	let mainURL = "https://wind-bow.glitch.me/twitch-api/";
 	var channelData;
-	for ( let i = 0; i < CHANNELS.length; i++ ) { // cycle through the CHANNELS and make an API call for all of them
-		let idCount = CHANNELS[ i ]; // var to hold put the channel name into .card id
 
+	// commented out the first AJAX call to just get stream status
 
-		$.getJSON( mainURL + "channels/" + CHANNELS[ i ], function( data ) {
-
-			console.dir( data );
-			channelData = data;
-			let channelURL = data.url;
-
-
-			let cards = `<div class="cards" id="${idCount}">
-			<img src="${data.profile_banner}" class="img-fluid">
-			<p>Status:</p>
-			<p class="isOffline" id="${i}">${streamStatus}  </p>
-			<p class="mr-2">  ${data.display_name}  </p>
-			</div>`;
-
-			// add the next card to the end of #showcase
-			$( "#showcase" ).append( cards );
-
-			// add eventListener to each .card for a click to take them to the Twitch channel
-			document.getElementById( idCount ).addEventListener( "click", function() {
-				window.open( channelURL );
-			} );
-
-			// render new box elements for each channel
-
-		} ); // end of .getJSON() function
-
-
-	} // end for...loop for first AJAX call
+	// for ( let i = 0; i < CHANNELS.length; i++ ) { // cycle through the CHANNELS and make an API call for all of them
+	// 	let idCount = CHANNELS[ i ]; // var to hold put the channel name into .card id
+	//
+	//
+	// 	$.getJSON( mainURL + "channels/" + CHANNELS[ i ], function( data ) {
+	//
+	// 		console.dir( data );
+	// 		channelData = data;
+	// 		let channelURL = data.url;
+	//
+	//
+	// 		let cards = `<div class="cards" id="${idCount}">
+	// 		<img src="${data.profile_banner}" class="img-fluid">
+	// 		<p>Status:</p>
+	// 		<p class="isOffline" id="${i}">${streamStatus}  </p>
+	// 		<p class="mr-2">  ${data.display_name}  </p>
+	// 		</div>`;
+	//
+	// 		// add the next card to the end of #showcase
+	// 		// $( "#showcase" ).append( cards );
+	//
+	// 		// add eventListener to each .card for a click to take them to the Twitch channel
+	// 		document.getElementById( idCount ).addEventListener( "click", function() {
+	// 			window.open( channelURL );
+	// 		} );
+	//
+	// 		// render new box elements for each channel
+	//
+	// 	} ); // end of .getJSON() function
+	//
+	//
+	// } // end for...loop for first AJAX call
 
 	for ( let j = 0; j < CHANNELS.length; j++ ) {
 		let idCount = CHANNELS[ j ];
 		$.getJSON( mainURL + "streams/" + CHANNELS[ j ], function( data ) {
-
+			let cardStatus = document.getElementsByClassName("status");
 			console.dir( data );
 			if ( !data.stream == true ) {
-				streamStatus = "Offline";
+				cardStatus[j].style = "color: red"
+				cardStatus[j].innerText = "Offline"
 
-				// document.getElementById(idCount)
 			} else {
-				streamStatus = data.stream.game;
+				cardStatus[j].style = "color: green"
+				cardStatus[j].innerText = data.stream.game;
 			}
 
-			if (streamStatus !== "Offline") {
-				// document.getElementById(j).classList.add(".isOnline" );
-				// $("#status").removeClass(".isOnline");
-				// $("#status").addClass(".isOffline");
-
-			}
+			document.getElementById(`card${ j + 1}`).addEventListener("click", function() {
+				window.open("https://www.twitch.tv/" + idCount);
+			})
 
 
 		} ); // end second AJAX call to get the status of the live stream
