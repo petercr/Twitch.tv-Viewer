59bff639c9b274bfb36becdc85f1e623668b7f90
Peter Cruckshank <petecruckshank@gmail.com>
2018-01-20 10:27:04 -0500

    added in CHANNELS.forEach() loop as test instead of for...loop Also let ESLint fix some of the spacing and ; errors

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 	let mainURL = "https://wind-bow.glitch.me/twitch-api/";
 	var channelData;
 
+
+	// test loop to see if I can refractor for loop into a .forEach loop
+	CHANNELS.forEach( ( value, index ) => {
+		console.log( value + " " + index );
+	} );
+
 	// commented out the first AJAX call to just get stream status
 
 	for ( let i = 0; i < CHANNELS.length; i++ ) { // cycle through the CHANNELS and make an API call for all of them
@@ -21,11 +27,12 @@ const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 
 		$.getJSON( mainURL + "channels/" + CHANNELS[ i ], function( data ) {
 
+
 			console.dir( data );
 
 			var logoPictures = document.getElementsByTagName("img");
-			logoPictures[i + 1].src = data.logo;
-			console.log(logoPictures);
+			logoPictures[ i + 1 ].src = data.logo;
+			console.log( logoPictures);
 
 		} ); // end of .getJSON() function
 
@@ -35,20 +42,21 @@ const CHANNELS = [ "freecodecamp", "aws", "kyleshevlin", "barbarousking" ];
 	for ( let j = 0; j < CHANNELS.length; j++ ) {
 		let idCount = CHANNELS[ j ];
 		$.getJSON( mainURL + "streams/" + CHANNELS[ j ], function( data ) {
-			let cardStatus = document.getElementsByClassName("status");
+			let cardStatus = document.getElementsByClassName( "status" );
+
 			// console.dir( data );
 			if ( !data.stream == true ) { // if there is no stream then change color to red and text to Offline
-				cardStatus[j].style = "color: red"
-				cardStatus[j].innerText = "Offline"
+				cardStatus[ j ].style = "color: red";
+				cardStatus[ j ].innerText = "Offline";
 
 			} else {  // if there IS a stream then change color to green and text to Online
-				cardStatus[j].style = "color: green"
-				cardStatus[j].innerText = data.stream.game;
+				cardStatus[ j ].style = "color: green";
+				cardStatus[ j ].innerText = data.stream.game;
 			}
 
-			document.getElementById(`card${ j + 1}`).addEventListener("click", function() {
-				window.open("https://www.twitch.tv/" + idCount);
-			})
+			document.getElementById( `card${ j + 1}` ).addEventListener( "click", function() {
+				window.open( "https://www.twitch.tv/" + idCount );
+			});
 
 
 		} ); // end second AJAX call to get the status of the live stream
